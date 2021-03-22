d9adce787d338a2c8e191fdf61e8e2da4bee7d27
root <root@bastion.ocp4.example.com>
2021-03-23 03:00:14 +0800

    update to v4.6+ and BIOS & UEFI

--- roles/boot-for-rhcos/files/update_grub_for_rhcos.sh
+++ roles/boot-for-rhcos/files/update_grub_for_rhcos.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-grub2-mkconfig -o /boot/grub2/grub.cfg
-GRUB_NUM=`awk -F\' '$1=="menuentry " {print $2}' /etc/grub2.cfg | grep -v coreos | wc -l`
-sed -i "s#GRUB_DEFAULT=.*#GRUB_DEFAULT=$GRUB_NUM#" /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-sed -i "s#^   set default=.[0-9].#   set default='$GRUB_NUM'#" /boot/grub2/grub.cfg
+GRUB_NUM="'coreos'"
+
+if [ -n "$(grep -i 'coreos' /etc/*release)" ]; then
+    touch /etc/default/grub 
+    echo -e "GRUB_DEFAULT=$GRUB_NUM\nGRUB_ENABLE_BLSCFG=true" > /etc/default/grub
+    if [ -d /sys/firmware/efi ]; then
+        grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg
+    else
+        grub2-mkconfig -o /boot/grub2/grub.cfg
+    fi
+else
+    sed -i "s#GRUB_DEFAULT=.*#GRUB_DEFAULT=$GRUB_NUM#" /etc/default/grub || true
+    [ -d /sys/firmware/efi ] && grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg || grub2-mkconfig -o /boot/grub2/grub.cfg
+fi
